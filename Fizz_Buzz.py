efaa212b89a32bc16e83d7a93c1f40d7281afdfb
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-10-18 22:23:06 +0300

    Fizz_Buzz Challange done!

---
+++ Fizz_Buzz.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = [i for i in range(1, n+1)]
+        for m in range(0, len(answer)):
+          if answer[m] % 3 == 0 and answer[m] % 5 == 0:
+            answer[m] = "FizzBuzz"
+          elif answer[m] % 3 == 0:
+            answer[m] = "Fizz"
+          elif answer[m] % 5 == 0:
+            answer[m] = "Buzz"
+        return answer
+
+obj = Solution()
+print(obj.fizzBuzz(15))
